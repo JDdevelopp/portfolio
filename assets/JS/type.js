4a0577fb04ffc898f34f99d1b6f019c14a87a6c8
JDdevelopp <jagratadeb2007@gmail.com>
2022-08-11 11:47:36 +0530

    preloader

--- assets/JS/type.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".head_2", {
-    strings: ["blankdata","Web Development.","Sleeping.","Annoying My Friends.","Listening Music"],
-    typeSpeed: 100,
-    backSpeed: 25,
-    showCursor: false,
-    loop: true,
-})
